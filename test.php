8d3d3b9bb7cedd3eb7540f18e0fdeb2f6eb48fa6
“longyunfei” <532871613@qq.com>
2023-04-03 14:21:59 +0800

    test

--- test.php
+++ test.php
@@ -2,6 +2,6 @@
 
 require_once 'vendor/autoload.php';
 
-$class = new \Dxkjcomposer\PhpImap\Mailbox('{mail.dingstartech.com:993/imap/ssl}', 'phptest2@dingstartech.com', 'Ifun_123');
+$class = new \Dxkjcomposer\PhpImap\Mailbox('', '', '');
 
-print_r($class->getMail(223));
+print_r($class->getMail(''));
